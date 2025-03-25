b9797fb95662d931eb030efdecf421eb6e1b1a53
Diane-4P <f39xy05z@students.codeinstitute.net>
2025-03-25 18:26:10 +0000

    Basic structure of JavaScript

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/**
+ * Modal of instructions of how to play
+ */
+
+/**
+ * Questions and Answers
+ */
+/**
+ * Year One
+ */
+
+/**
+ * Year Two
+ */
+
+/**
+ * Year Three
+ */
+
+/**
+ * Wait for the DOM to finish loading before running the game
+ * Inspiration from the Love Maths project
+ * Add event to select level
+ */
+
+/**
+ * Removes the levels to show the questions and answers for the level chosen
+ * 
+ */
+
+/**
+ * Increase correct or incorrect score based on answer
+ * Go to next question
+ */
+
+/**
+ * Increasing the correct and incorrect score
+ */
